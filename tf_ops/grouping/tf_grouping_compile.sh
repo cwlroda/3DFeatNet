fbd0d395672b476f84eb48e78b137bfec578f843
tianyilim <0.tianyi.lim@gmail.com>
2021-07-29 10:56:49 +0800

    added autodetection of cuda version

--- tf_ops/grouping/tf_grouping_compile.sh
+++ tf_ops/grouping/tf_grouping_compile.sh
@@ -1,5 +1,12 @@
 #/bin/bash
-/usr/local/cuda-10.0/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+# Obtain cuda version (assume that nvcc has been added to path)
+set -e
+NVCC_VER=`nvcc --version`
+NVCC_VER=`echo $NVCC_VER | cut -d "_" -f 7`
+NVCC_VER="${NVCC_VER:0:4}"  # Highly hardcoded
+
+/usr/local/cuda-${NVCC_VER}/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
@@ -7,5 +14,5 @@ TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC \
   -I ${TF_INC} \
   -I ${TF_INC}/external/nsync/public \
-  -I /usr/local/cuda-10.0/include -lcudart -L /usr/local/cuda-10.0/lib64/ \
-  -L${TF_LIB} -l:libtensorflow_framework.so.1 -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+  -I /usr/local/cuda-${NVCC_VER}/include -lcudart -L /usr/local/cuda-${NVCC_VER}/lib64/ \
+  -L${TF_LIB} -l:libtensorflow_framework.so.2 -O2 -D_GLIBCXX_USE_CXX11_ABI=0
--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -1,5 +1,11 @@
 #/bin/bash
-/usr/local/cuda-10.0/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+set -e
+NVCC_VER=`nvcc --version`
+NVCC_VER=`echo $NVCC_VER | cut -d "_" -f 7`
+NVCC_VER="${NVCC_VER:0:4}"  # Highly hardcoded
+
+/usr/local/cuda-${NVCC_VER}/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
@@ -7,5 +13,5 @@ TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC \
   -I ${TF_INC} \
   -I ${TF_INC}/external/nsync/public \
-  -I /usr/local/cuda-10.0/include -lcudart -L /usr/local/cuda-10.0/lib64/ \
-  -L${TF_LIB} -l:libtensorflow_framework.so.1 -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+  -I /usr/local/cuda-${NVCC_VER}/include -lcudart -L /usr/local/cuda-${NVCC_VER}/lib64/ \
+  -L${TF_LIB} -l:libtensorflow_framework.so.2 -O2 -D_GLIBCXX_USE_CXX11_ABI=0
