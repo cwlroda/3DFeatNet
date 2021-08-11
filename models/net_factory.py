41c5023165b795ff7df3ff105089104bb09f0cee
tianyilim <0.tianyi.lim@gmail.com>
2021-08-11 14:08:34 +0800

    net_factory includes tf2 version of 3dFn

--- models/net_factory.py
+++ models/net_factory.py
@@ -1,6 +1,7 @@
-from models import feat3dnet
+from models import feat3dnet, feat3dnet_tf2
 
 networks_map = {'3DFeatNet': feat3dnet.Feat3dNet,
+                '3DFeatNet_tf2': feat3dnet_tf2.Feat3dNet
                 }
 
 
